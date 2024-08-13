971f9f195678995ad03fe5d73ad30c8089c8f4b9
Reshabcsst <reshabn.csst@gmail.com>
2024-08-13 18:49:54 +0530

    uno

--- src/Components/Component/AboutComponents/History.jsx
+++ src/Components/Component/AboutComponents/History.jsx
@@ -14,14 +14,36 @@ const History = () => {
                     </div>
                 </div>
                 <div className="st">
-                    <h3>2004</h3>
+                    <h3>2007</h3>
                     <div className="dt">
                         <h4>LBS of KMC</h4>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque aperiam eaque est fuga sint, amet sapiente incidunt, corrupti beatae alias fugiat odit tempore, porro necessitatibus quos blanditiis esse quasi laboriosam maiores? Ad, vitae placeat accusantium pariatur voluptatibus atque magnam soluta veniam earum cumque quibusdam temporibus harum ut voluptatem. Perferendis in, sapiente consectetur tempora, laudantium aspernatur modi quae minus impedit, numquam et unde tenetur sed voluptatem repudiandae dolores blanditiis commodi. Ipsum asperiores pariatur impedit minus aut error quidem quas laboriosam dicta exercitationem neque temporibus, expedita magnam dolores doloribus corporis quia reprehenderit iste alias cum repellendus ullam ad. Eveniet corporis maiores totam.</p>
                     </div>
                 </div>
                  <div className="st">
-                    <h3>2004</h3>
+                    <h3>2010</h3>
+                    <div className="dt">
+                        <h4>LBS of KMC</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque aperiam eaque est fuga sint, amet sapiente incidunt, corrupti beatae alias fugiat odit tempore, porro necessitatibus quos blanditiis esse quasi laboriosam maiores? Ad, vitae placeat accusantium pariatur voluptatibus atque magnam soluta veniam earum cumque quibusdam temporibus harum ut voluptatem. Perferendis in, sapiente consectetur tempora, laudantium aspernatur modi quae minus impedit, numquam et unde tenetur sed voluptatem repudiandae dolores blanditiis commodi. Ipsum asperiores pariatur impedit minus aut error quidem quas laboriosam dicta exercitationem neque temporibus, expedita magnam dolores doloribus corporis quia reprehenderit iste alias cum repellendus ullam ad. Eveniet corporis maiores totam.</p>
+                    </div>
+                </div>
+                <div className="st">
+                    <h3>2015</h3>
+                    <div className="dt">
+                        <h4>LBS of KMC</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque aperiam eaque est fuga sint, amet sapiente incidunt, corrupti beatae alias fugiat odit tempore, porro necessitatibus quos blanditiis esse quasi laboriosam maiores? Ad, vitae placeat accusantium pariatur voluptatibus atque magnam soluta veniam earum cumque quibusdam temporibus harum ut voluptatem. Perferendis in, sapiente consectetur tempora, laudantium aspernatur modi quae minus impedit, numquam et unde tenetur sed voluptatem repudiandae dolores blanditiis commodi. Ipsum asperiores pariatur impedit minus aut error quidem quas laboriosam dicta exercitationem neque temporibus, expedita magnam dolores doloribus corporis quia reprehenderit iste alias cum repellendus ullam ad. Eveniet corporis maiores totam.</p>
+                    </div>
+                </div>
+                <div className="st">
+                    <h3>2020</h3>
+                    <div className="dt">
+                        <h4>LBS of KMC</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque aperiam eaque est fuga sint, amet sapiente incidunt, corrupti beatae alias fugiat odit tempore, porro necessitatibus quos blanditiis esse quasi laboriosam maiores? Ad, vitae placeat accusantium pariatur voluptatibus atque magnam soluta veniam earum cumque quibusdam temporibus harum ut voluptatem. Perferendis in, sapiente consectetur tempora, laudantium aspernatur modi quae minus impedit, numquam et unde tenetur sed voluptatem repudiandae dolores blanditiis commodi. Ipsum asperiores pariatur impedit minus aut error quidem quas laboriosam dicta exercitationem neque temporibus, expedita magnam dolores doloribus corporis quia reprehenderit iste alias cum repellendus ullam ad. Eveniet corporis maiores totam.</p>
+                    </div>
+                </div>
+
+                <div className="st">
+                    <h3>2024</h3>
                     <div className="dt">
                         <h4>LBS of KMC</h4>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque aperiam eaque est fuga sint, amet sapiente incidunt, corrupti beatae alias fugiat odit tempore, porro necessitatibus quos blanditiis esse quasi laboriosam maiores? Ad, vitae placeat accusantium pariatur voluptatibus atque magnam soluta veniam earum cumque quibusdam temporibus harum ut voluptatem. Perferendis in, sapiente consectetur tempora, laudantium aspernatur modi quae minus impedit, numquam et unde tenetur sed voluptatem repudiandae dolores blanditiis commodi. Ipsum asperiores pariatur impedit minus aut error quidem quas laboriosam dicta exercitationem neque temporibus, expedita magnam dolores doloribus corporis quia reprehenderit iste alias cum repellendus ullam ad. Eveniet corporis maiores totam.</p>
