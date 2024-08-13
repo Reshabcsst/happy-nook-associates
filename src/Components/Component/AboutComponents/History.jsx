b084126586f6fc6aea8d13a29b946947f8c6a7cd
Reshabcsst <reshabn.csst@gmail.com>
2024-08-13 18:43:01 +0530

    uno

--- src/Components/Component/AboutComponents/History.jsx
+++ src/Components/Component/AboutComponents/History.jsx
@@ -2,11 +2,25 @@ import React from 'react';
 
 const History = () => {
     return (
-        <div className='history container'>
+        <div data-aos='fade-right' className='history container'>
             <h2>Our History<span>.</span></h2>
             <div className="tree">
 
                 <div className="st">
+                    <h3>2004</h3>
+                    <div className="dt">
+                        <h4>LBS of KMC</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque aperiam eaque est fuga sint, amet sapiente incidunt, corrupti beatae alias fugiat odit tempore, porro necessitatibus quos blanditiis esse quasi laboriosam maiores? Ad, vitae placeat accusantium pariatur voluptatibus atque magnam soluta veniam earum cumque quibusdam temporibus harum ut voluptatem. Perferendis in, sapiente consectetur tempora, laudantium aspernatur modi quae minus impedit, numquam et unde tenetur sed voluptatem repudiandae dolores blanditiis commodi. Ipsum asperiores pariatur impedit minus aut error quidem quas laboriosam dicta exercitationem neque temporibus, expedita magnam dolores doloribus corporis quia reprehenderit iste alias cum repellendus ullam ad. Eveniet corporis maiores totam.</p>
+                    </div>
+                </div>
+                <div className="st">
+                    <h3>2004</h3>
+                    <div className="dt">
+                        <h4>LBS of KMC</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque aperiam eaque est fuga sint, amet sapiente incidunt, corrupti beatae alias fugiat odit tempore, porro necessitatibus quos blanditiis esse quasi laboriosam maiores? Ad, vitae placeat accusantium pariatur voluptatibus atque magnam soluta veniam earum cumque quibusdam temporibus harum ut voluptatem. Perferendis in, sapiente consectetur tempora, laudantium aspernatur modi quae minus impedit, numquam et unde tenetur sed voluptatem repudiandae dolores blanditiis commodi. Ipsum asperiores pariatur impedit minus aut error quidem quas laboriosam dicta exercitationem neque temporibus, expedita magnam dolores doloribus corporis quia reprehenderit iste alias cum repellendus ullam ad. Eveniet corporis maiores totam.</p>
+                    </div>
+                </div>
+                 <div className="st">
                     <h3>2004</h3>
                     <div className="dt">
                         <h4>LBS of KMC</h4>
