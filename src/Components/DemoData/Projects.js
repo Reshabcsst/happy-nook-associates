a3d292841533f3ab6eca008d5412be7cbebd26f6
Reshabcsst <reshabn.csst@gmail.com>
2024-08-20 18:47:32 +0530

    uno

--- src/Components/DemoData/Projects.js
+++ src/Components/DemoData/Projects.js
@@ -1,45 +1,39 @@
 import Project1 from '../Assets/Royal.jpeg';
 import Project2 from '../Assets/Rupal Heights.jpeg';
-import Project3 from '../Assets/Project3.jpg';
-import Project4 from '../Assets/Project4.jpg';
-import Project5 from '../Assets/Project5.jpg';
-import Project6 from '../Assets/Project6.jpg';
+import Project3 from '../Assets/All Projects/2222.jpg';
+import Project5 from '../Assets/All Projects/6th floor.jpg';
+import Project6 from '../Assets/All Projects/93Hari Ghoshe Street.jpg';
+import Project7 from '../Assets/All Projects/FINAL 15a CHIDAM MUDI LANE copy.jpg';
+import Project8 from '../Assets/All Projects/FINAL COPY RAJA NABAKRISNA STREET.jpg';
+import Project9 from '../Assets/All Projects/FINAL PHOTO.jpg';
+import Project10 from '../Assets/All Projects/Hari Ghoshel edit glass.jpg';
+import Project11 from '../Assets/All Projects/IMG-20170401-WA0000.jpg';
+import Project13 from '../Assets/All Projects/Raja nabakrisna Street final copy.jpg';
+import Project15 from '../Assets/All Projects/akshoy kumar dutta sarani.jpg';
+import Project16 from '../Assets/All Projects/back side copy.jpg';
+import Project17 from '../Assets/All Projects/for send final.jpg';
+import Project18 from '../Assets/All Projects/new view 5 flor extra change file copy.jpg';
+import Project19 from '../Assets/All Projects/photo re edit 1.jpg';
 
 const Projects = [
     { id: 1, image: Project1, name: 'Royal Hotel', category: 'Apartments/Condos' },
     { id: 2, image: Project2, name: 'Rupal Heights', category: 'Religious Facilities' },
     { id: 3, image: Project3, name: 'Project Three', category: 'Office Buildings' },
-    { id: 4, image: Project4, name: 'Project Four', category: 'Apartments/Condos' },
     { id: 5, image: Project5, name: 'Project Five', category: 'Religious Facilities' },
-    { id: 6, image: Project6, name: 'Project Six', category: 'Religious Facilities' },
+    { id: 6, image: Project6, name: '93Hari Ghoshe Street', category: 'Religious Facilities' },
 
-    { id: 7, image: Project1, name: 'Project One', category: 'Hotels/Resorts' },
-    { id: 8, image: Project2, name: 'Project Two', category: 'Apartments/Condos' },
-    { id: 9, image: Project3, name: 'Project Three', category: 'Apartments/Condos' },
-    { id: 10, image: Project4, name: 'Project Four', category: 'Hotels/Resorts' },
-    { id: 11, image: Project5, name: 'Project Five', category: 'Apartments/Condos' },
-    { id: 12, image: Project6, name: 'Project Six', category: 'Religious Facilities' },
+    { id: 7, image: Project7, name: '15a CHIDAM MUDI LANE', category: 'Hotels/Resorts' },
+    { id: 8, image: Project8, name: 'Project Two', category: 'Apartments/Condos' },
+    { id: 9, image: Project9, name: 'Project Three', category: 'Apartments/Condos' },
+    { id: 10, image: Project10, name: 'Project Four', category: 'Hotels/Resorts' },
+    { id: 11, image: Project11, name: 'Project Five', category: 'Apartments/Condos' },
 
-
-    { id: 13, image: Project1, name: 'Project One', category: 'Apartments/Condos' },
-    { id: 14, image: Project2, name: 'Project Two', category: 'Apartments/Condos' },
-    { id: 15, image: Project3, name: 'Project Three', category: 'Retrofit/Restoration' },
-    { id: 16, image: Project4, name: 'Project Four', category: 'Office Buildings' },
-    { id: 17, image: Project5, name: 'Project Five', category: 'Hotels/Resorts' },
-    { id: 18, image: Project6, name: 'Project Six', category: 'Apartments/Condos' },
-
-    { id: 19, image: Project1, name: 'Project One', category: 'Religious Facilities' },
-    { id: 20, image: Project2, name: 'Project Two', category: 'Apartments/Condos' },
-    { id: 21, image: Project3, name: 'Project Three', category: 'Office Buildings' },
-    { id: 22, image: Project4, name: 'Project Four', category: 'Retrofit/Restoration' },
-    { id: 23, image: Project5, name: 'Project Five', category: 'Apartments/Condos' },
-    { id: 24, image: Project6, name: 'Project Six', category: 'Office Buildings' },
-
-
-    { id: 25, image: Project3, name: 'Project Three', category: 'Mixed Use' },
-    { id: 26, image: Project4, name: 'Project Four', category: 'Mixed Use' },
-    { id: 27, image: Project5, name: 'Project Five', category: 'Apartments/Condos' },
-    { id: 28, image: Project6, name: 'Project Six', category: 'Mixed Use' },
+    { id: 13, image: Project13, name: 'Raja nabakrisna Street', category: 'Apartments/Condos' },
+    { id: 15, image: Project15, name: 'Project Three', category: 'Retrofit/Restoration' },
+    { id: 16, image: Project16, name: 'Project Four', category: 'Office Buildings' },
+    { id: 17, image: Project17, name: 'Project Five', category: 'Hotels/Resorts' },
+    { id: 18, image: Project18, name: 'Project Six', category: 'Apartments/Condos' },
+    { id: 19, image: Project19, name: 'Project One', category: 'Religious Facilities' },
 ];
 
 export default Projects;
